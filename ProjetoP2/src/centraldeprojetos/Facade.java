5ae627c532231be6dddd77d4aa1a4f7ed69c0418
Yuri Kelvin <yuri.silva@ccc.ufcg.edu.br>
2017-03-10 20:49:48 -0300

    Adicionando arquivos ao sistema

--- ProjetoP2/src/centraldeprojetos/Facade.java
+++ ProjetoP2/src/centraldeprojetos/Facade.java
@@ -1,26 +1,30 @@
 package centraldeprojetos;
 
-import easyaccept.EasyAccept;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+//import easyaccept.EasyAccept;
 import exception.CadastroException;
 import exception.ValidacaoException;
+import participacao.ParticipacaoController;
 import pessoas.Pessoa;
 import pessoas.PessoaController;
+import projetos.ProjetoController;
 import validacao.ValidaPessoa;
 
 public class Facade {
 
-	private PessoaController meuPessoa;
-	//private ProjetoController meuProjeto;
-	
-	public Facade(){
-		meuPessoa = new PessoaController();
-		//meuProjeto = new ProjetoController();
-	}
+	private PessoaController pessoaController;
+	private ParticipacaoController participacaoController;
+	private ProjetoController projetosController;
 	
 	public String cadastraPessoa(String cpf, String nome, String email) throws ValidacaoException, CadastroException{
 		
 		try{
-			return meuPessoa.cadastraPessoa(cpf, nome, email);
+			return pessoaController.cadastraPessoa(cpf, nome, email);
 		}catch(CadastroException e){
 			throw new CadastroException("Erro no cadastro de pessoa: " + e.getMessage());
 		}catch(ValidacaoException e){
@@ -31,7 +35,7 @@ public class Facade {
 	public String getInfoPessoa(String cpf, String atributo) throws ValidacaoException, IllegalArgumentException, CadastroException {
 		
 		try{
-			return meuPessoa.getInfoPessoa(cpf, atributo);
+			return pessoaController.getInfoPessoa(cpf, atributo);
 		}catch(ValidacaoException e){
 			throw new ValidacaoException("Erro na consulta de pessoa: " + e.getMessage());
 		}catch(IllegalArgumentException e){
@@ -44,7 +48,7 @@ public class Facade {
 	public void editaPessoa(String cpf, String atributo, String novoValor) throws CadastroException, ValidacaoException, IllegalArgumentException{
 		
 		try{
-			meuPessoa.editaPessoa(cpf, atributo, novoValor);
+			pessoaController.editaPessoa(cpf, atributo, novoValor);
 		}catch(ValidacaoException e){
 			throw new ValidacaoException("Erro na atualizacao de pessoa: " + e.getMessage());
 		}catch(CadastroException e){
@@ -57,7 +61,7 @@ public class Facade {
 	public void removePessoa(String cpf) throws CadastroException, ValidacaoException {
 		
 		try{
-			meuPessoa.removePessoa(cpf);
+			pessoaController.removePessoa(cpf);
 		}catch(ValidacaoException e){
 			throw new ValidacaoException("Erro na remocao de pessoa: " + e.getMessage());
 		}catch(CadastroException e){
@@ -66,15 +70,92 @@ public class Facade {
 	}
 	
 	public void iniciaSistema() throws Exception {
-		// posteriormente ser implementado
+		
+		this.pessoaController = (PessoaController) restauraPessoaController("pessoas.ser");
+		this.projetosController = (ProjetoController) restauraProjetoController("projetos.ser");
+		this.participacaoController = (ParticipacaoController) restauraParticipacaoController("participacoes.ser");
+		
 	}
 	
-	public void fechaSistema() throws Exception {
-		// posteriormente ser implementado
+	private PessoaController restauraPessoaController(String caminho) {
+		PessoaController pessoaController = null;
+		
+		try{
+			try(ObjectInputStream arqObjectos = new ObjectInputStream(new FileInputStream(caminho))){
+				pessoaController = (PessoaController) arqObjectos.readObject();
+			}
+		}catch(ClassNotFoundException | IOException e){
+			pessoaController = new PessoaController();
+		}
+		return pessoaController;
+
 	}
+	
+	private ProjetoController restauraProjetoController(String caminho) {
+		ProjetoController projetoController = null;
+		try{
+			try(ObjectInputStream arqObjectos = new ObjectInputStream(new FileInputStream(caminho))){
+				projetoController = (ProjetoController) arqObjectos.readObject();
+			}
+		}catch(ClassNotFoundException | IOException e){
+			projetoController = new ProjetoController();
+		}
+		return projetoController;
+
+	}
+	
+	private ParticipacaoController restauraParticipacaoController(String caminho) {
+		ParticipacaoController participacaoController = null;
+
+		try{
+			try(ObjectInputStream arqObjectos = new ObjectInputStream(new FileInputStream(caminho))){
+				participacaoController = (ParticipacaoController) arqObjectos.readObject();
+			}
+		}catch(ClassNotFoundException | IOException e){
+			participacaoController = new ParticipacaoController();
+		}
+		return participacaoController;
+
+	}
+	
+
+	public void fechaSistema() throws IOException {
+		
+		try{
+			try(ObjectOutputStream arqObjectos = new ObjectOutputStream(new FileOutputStream("pessoas.ser"))){
+				arqObjectos.writeObject(this.pessoaController);
+			}
+		}catch(IOException e){
+			throw new IOException(e);
+		}
+		
+		try{
+			try(ObjectOutputStream arqObjectos = new ObjectOutputStream(new FileOutputStream("projetos.ser"))){
+				arqObjectos.writeObject(this.projetosController);
+			}
+		}catch(IOException e){
+			throw new IOException(e);
+		}
+		
+		try{
+			try(ObjectOutputStream arqObjectos = new ObjectOutputStream(new FileOutputStream("participacoes.ser"))){
+				arqObjectos.writeObject(this.participacaoController);
+			}
+		}catch(IOException e){
+			throw new IOException(e);
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+
 	
 	public static void main(String[] args) {
 		args = new String[] {"centraldeprojetos.Facade", "acceptance_tests/us1_test.txt", "acceptance_tests/us1_test_exception.txt"};
-		EasyAccept.main(args);
+		//EasyAccept.main(args);
 }
 }
